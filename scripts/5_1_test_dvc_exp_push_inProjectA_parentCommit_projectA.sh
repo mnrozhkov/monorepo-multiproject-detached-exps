758d71ef4268aa3e96961de408b6256e3b1273f6
Mikhail Rozhkov <mnrozhkov@gmail.com>
2024-01-17 12:10:54 +0100

    Add 5_1 .. 5_3 scripts

---
+++ scripts/5_1_test_dvc_exp_push_inProjectA_parentCommit_projectA.sh
@@ -0,0 +1,19 @@
+# 5.1 Test DVC experiment push in Project A
+# - use case: dvc exp push 
+# - parent commit: commited in same subdir (project_a) & NOT PUSHED
+
+# (step 1) Prepare the starting commit - in project_a 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 5_1_test_dvc_exp_push_inProjectA.sh" >> project_a/some_projectA_content
+git add project_a/some_projectA_content
+git commit -m "$(date '+%H:%M:%S') - 5_1_test_dvc_exp_push_inProjectA.sh"
+# git push origin main  # Comment this line to simulate NOT PUSHED parent commit scenario
+
+# (step 2) Clean experiments in A
+cd project_a
+dvc exp remove -A
+
+# (step 3) Run expriment in A
+dvc exp run -f
+
+# (step 4) Push exp ref 
+dvc exp push origin -A
---
+++ scripts/5_3_test_dvc_exp_push_inProjectA_parentCommit_outside_inProjectB.sh
@@ -0,0 +1,19 @@
+# 5.2 Test DVC experiment push in Project A
+# - use case: dvc exp push 
+# - parent commit: outside (project_b)
+
+# (step 1) Prepare the starting commit - in project_a 
+echo "$(date '+%Y-%m-%d %H:%M:%S') - 5_3_test_dvc_exp_push_inProjectA_parentCommit_outside_inProjectB.sh" >> project_b/some_projectB_content
+git add project_b/some_projectB_content
+git commit -m "$(date '+%H:%M:%S') - 5_3_test_dvc_exp_push_inProjectA_parentCommit_outside_inProjectB.sh"
+git push origin main
+
+# (step 2) Clean experiments in A
+cd project_a
+dvc exp remove -A
+
+# (step 3) Run expriment in A
+dvc exp run -f
+
+# (step 4) Push exp ref 
+dvc exp push origin -A
