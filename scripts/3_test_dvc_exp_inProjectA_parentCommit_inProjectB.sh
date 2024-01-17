c37c887b8c4985abcb04b7da32ebaecb1a329102
Mikhail Rozhkov <mnrozhkov@gmail.com>
2024-01-17 12:20:41 +0100

    Update scripts/3_test..

--- scripts/3_test_dvc_exp_inProjectA_parentCommit_inProjectB.sh
+++ scripts/3_test_dvc_exp_inProjectA_parentCommit_inProjectB.sh
@@ -1,26 +1,15 @@
-# Add a new line to some_root_content file
+# (step 1) Prepare the starting commit - in project_b 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh" >> project_b/some_projectB_content
-
-# Commit changes to Git
 git add project_b/some_projectB_content
 git commit -m "3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh"
-
-# Change directory to dproject_a
-cd project_a
-
-# Run dvc experiment
-dvc exp run -f
-
-
-# (step 1) Commit changes to Git
-# -> no commit here, screenshots are taken 
-
-
-# (step 2) Push changes to remote repository
 git push origin main # -> only changes in project_b/some_projectB_content
 
+# (step 2) Run expriment in A
+cd project_a
+dvc exp run -f
 
 # (step 3) Push DVC experiment from Project A
 git add project_a/*
 git commit -m "Commit dvc exp: 3-test_dvc_exp_inProjectA_parentCommit_inProjectB.sh"
 git push origin main 
+
