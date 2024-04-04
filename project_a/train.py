669ff33d8c0f1d7c3e9942baa946424127d92fd0
Mikhail <mnrozhkov@gmail.com>
2024-04-04 19:38:58 +0200

    project_a: register model

--- project_a/train.py
+++ project_a/train.py
@@ -1,9 +1,13 @@
+import numpy as np
 import random
 import time
 from dvclive import Live
 from dvc.api import params_show
 
 params = params_show()
+
+with open('model.npy', 'wb') as f:
+    np.save(f, np.random.randint(2, size=100))
 
 time.sleep(1)
 with Live(save_dvc_exp=True) as live:
@@ -14,3 +18,10 @@ with Live(save_dvc_exp=True) as live:
         live.log_metric("bar", params["epochs"] - (i + random.random()))
         live.next_step()
         time.sleep(params["sleep"])
+
+    live.log_artifact(
+        'model.npy',
+        type='model',
+        name='model_project_a', 
+    )
+
